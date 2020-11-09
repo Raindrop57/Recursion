59895d77b43114aa844129dca34c5c2a8e032865
Raindrop57 <julian.c.schima-1@ou.edu>
2020-11-09 17:02:04 -0600

    Add FileSize class

---
+++ src/FileSize.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class FileSize {
+	
+	/*
+	 * Directory
+	 * |
+	 * | file1 file2 file3 dir1
+	 * 						|
+	 * 						file4 file5 dir2
+	 * 										|
+	 * 										file 6
+	 */
+	
+	public static long getSize(File file) {
+		long size = 0;
+		// if file => get size
+		// if directory => get all files, and call getSize on each file
+		if (file.isDirectory()) {
+			File[] files = file.listFiles();
+			List<File> xFiles = new LinkedList<File>(Arrays.asList(files));
+			for (File f : xFiles) {
+				size += getSize(f);
+			}
+		}
+		else {
+			size += file.length();
+		}
+		
+		return size;
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please give directory for a file");
+		String str = input.nextLine();
+		System.out.println("The size is: " + getSize(new File(str)));
+		input.close();
+	}
+
+}
